f27ad153b6100872778da9f3383df8cada504455
Mitzy Onaolapo <rios_mit@yahoo.com>
2024-02-18 00:38:47 -0800

    missing markers

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 let map;
+let markers = [];
 
-async function getCoords(){
-    const pos = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
-    return [pos.coords.latitude, pos.coords.longitude]
-    
+//Generating a map
+
+async function createMap() {
+    let coords = await getCoords();
+    map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 14,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // User Marker
+    if (coords) {
+        let marker = L.marker(coords).addTo(map);
+        marker.bindPopup("You are Here").openPopup();
+    }
+    // Search Function for business type
+
+  //  document.getElementById('submit').addEventListener('click', function () {
+  //      selectBusinessType();
+   // });
 }
 
-getCoords().then(coords => {console.log(coords)
-  map = L.map('map').setView(coords, 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 14,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-                                                              
- console.log(coords)
-});
+//Actions 
+async function getCoords() {
+   const pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+    return [pos.coords.latitude, pos.coords.longitude];
+}
 
-// Marker
-getCoords().then(coords => {
-    if (coords) {
-    let marker = L.marker(coords).addTo(map);
-    marker.bindPopup("You are Here").openPopup();
-    }
-})
-
-
-//Search Function for business type
-
-document.getElementById('submit').addEventListener('click', function() {
-    selectBusinessType()
-
-})
+// ... (your existing code)
 
 async function selectBusinessType (){
     const options = {
@@ -41,19 +41,70 @@ async function selectBusinessType (){
         Accept: 'application/json',
         Authorization: 'fsq3P8wo1bQwkPgXoTJP56jjL/1bUtKoWC5BYf2RGYhvDKg='
         }
-    }
-    let limit = 5
-    let [lat, lon]= await getCoords();
+    };
+    let limit = 5;
 
-    let business = 'coffee';'hotel';'restaurant';'market';
-    let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+    let [lat, lng]= await getCoords();
+    let businessDropdown = document.getElementById('businessTypeDropdown');
+    let selectedBusiness = businessDropdown.value;
+
+    let business = ['coffee','restaurant','hotel','market'];
+    let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lng}`, options);
     let data = await response.text();
     let parsedData = JSON.parse(data);
-    let businesses = parsedData.results;
+    let businesses= parsedData.results;
+    return businesses
 
-    console.log(businesses)
-
+clearMarkers()
+    addMarkers()
 }
 
-// Search API item
+function processBusinesses(data) {
+	let businesses = data.map((element) => {
+		let location = {
+			name: element.name,
+			lat: element.location.lat,
+			long: element.location.lng,
+		};
+		return location
+	})
+	return businesses
+}
 
+
+function addMarkers(businesses) {
+    console.log('adding markers')
+
+    for (let i = 0; i < businesses.length; i++) {
+        const location = businesses[i].location;
+        if (location && location.lat && location.lng) {
+            markers.push(
+                L.marker([
+                    location.lat,
+                    location.lng,
+                ])
+                    .bindPopup(`<p1>${businesses[i].name}</p1>`)
+                    .addTo(map)
+            );
+        }
+    }
+}
+
+function clearMarkers() {
+    console.log('Clearing Markers')
+    markers.forEach((marker) => {
+        map.removeLayer(marker);
+    });
+    markers = [];
+}
+
+document.getElementById('submit').addEventListener('click', async function() {
+    let data = await selectBusinessType();
+
+    //Add Markers
+    let businesses = processBusinesses(data);
+    clearMarkers();
+    addMarkers(businesses);
+});
+// window load
+window.onload = createMap()
